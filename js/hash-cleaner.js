6e26cb320b1cbb4bd5f060bea801bf25119f89da
thefox3490 <shilnikovnikita63@gmail.com>
2024-06-25 08:45:45 +0400

    Remove old site files

--- js/hash-cleaner.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener('hashchange', (e)=>{
-    history.pushState('', document.title, window.location.pathname);
-    })
--- js/mobile.js
+++
@@ -1,6 +0,0 @@
-$('document').ready(function(){
-    $('.header-burger, .header-call-btn').click(function(event) {
-        $('.header-burger,.header-line-2-contacts').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-});
